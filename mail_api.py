032bcef76a5d6f76a0694748063c8cdb96570974
baisefuel <s.bril03@gmail.com>
2023-12-29 21:26:12 +0500

    3 kt

---
+++ mail_api.py
@@ -0,0 +1,36 @@
+from email.mime.text import MIMEText
+from base64 import urlsafe_b64encode
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from email.mime.text import MIMEText
+from google.oauth2.credentials import Credentials
+
+from googleapiclient.errors import HttpError
+
+scopes=['https://www.googleapis.com/auth/gmail.send']
+credentials = Credentials.from_authorized_user_file("token.json", scopes)
+
+service = build('gmail', 'v1', credentials=credentials)
+
+
+def create_message(sender, to, subject, body):
+    message = MIMEText(body)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    raw_message = urlsafe_b64encode(message.as_string().encode("utf-8"))
+    print(raw_message)
+    return {
+        'raw': raw_message.decode("utf-8")
+    }
+
+
+def send_email(sender, to, subject, body):
+    message = create_message(sender, to, subject, body)
+    try:
+        service.users().messages().send(userId="me", body=message).execute()
+        print("Сообщение отправлено.")
+    except HttpError as error:
+        print(f"Возникла ошибка {error}")
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+from mail_api import send_email
 
 class DataStorage:
     def __init__(self):
@@ -7,13 +10,32 @@ class DataStorage:
         self.count_fine_value = tk.StringVar()
         self.deadline_fine_value = tk.StringVar()
 
-def get_entries(data_storage):
+def get_entries(data_storage, name_entry, email_entry, count_fine_entry, deadline_fine_entry):
     data_storage.name_value = name_entry.get()
     data_storage.email_value = email_entry.get()
     data_storage.count_fine_value = count_fine_entry.get()
     data_storage.deadline_fine_value = deadline_fine_entry.get()
 
     print(data_storage.name_value, data_storage.email_value, data_storage.count_fine_value, data_storage.deadline_fine_value)
+
+    send_email_now(data_storage)
+
+def send_email_now(data_storage):
+    name = data_storage.name_value
+    email = data_storage.email_value
+    count_of_debt = data_storage.count_fine_value
+    deadline = data_storage.deadline_fine_value
+    subject = "Долги перед УрФУ"
+    print(email)
+
+    body = f"Уважаемый {name}, \n "\
+            f"Ваша задолженность перед Уральским Федеральным Университетом составляет {count_of_debt} рублей \n "\
+            f"Требуем погасить долг до {deadline} \n" \
+            f"\n С уважением, деканат ИРИТ-РТФ'"
+    print(body)
+    
+    our_email = 'urfu-123@urfu-123.iam.gserviceaccount.com'
+    send_email(our_email, email, subject, body)
 
 def make_label(root, text):
     label = tk.Label(root, text=text)
@@ -22,18 +44,37 @@ def make_label(root, text):
     entry.pack(pady=10)
     return entry
 
+def open_csv(data_storage):
+    filename = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+    our_email = 'urfu-123@urfu-123.iam.gserviceaccount.com'
+    subject = "Долги перед УрФУ"
+    if filename:
+        file_csv = pd.read_csv(filename, delimiter=',')
+        file_csv = file_csv.astype(str)
+        try:
+            for columns, rows in file_csv.iterrows():
+                row_list = rows.values.tolist()
+                print(*row_list)
+                body = f"Уважаемый {row_list[0]}, \n "\
+                    f"Ваша задолженность перед Уральским Федеральным Университетом составляет {row_list[2]} рублей \n "\
+                    f"Требуем погасить долг до {row_list[3]} \n" \
+                    f"\n С уважением, деканат ИРИТ-РТФ'"
+                print(body)
+                send_email(our_email, row_list[1], subject, body)
+        except pd.errors.EmptyDataError:
+            print('CSV файл пуст')
+
 if __name__ == "__main__":
     root = tk.Tk()
 
     data_storage = DataStorage()
-    name_label = make_label(root, "Введите ФИО студента:")
-    email_label = make_label(root, "Введите электронную почту студента:")
-    count_fine_label = make_label(root, "Введите сумму задолженности:")
-    deadline_fine_label = make_label(root, "Введите дедлайн задолженности:")
-    button = tk.Button(root, text="Подтвердить", command=lambda: get_entries(data_storage))
+    name_entry = make_label(root, "Введите ФИО студента:")
+    email_entry = make_label(root, "Введите электронную почту студента:")
+    count_fine_entry = make_label(root, "Введите сумму задолженности:")
+    deadline_fine_entry = make_label(root, "Введите дедлайн задолженности:")
+    button = tk.Button(root, text="Подтвердить", command=lambda: get_entries(data_storage, name_entry, email_entry, count_fine_entry, deadline_fine_entry))
     button.pack()
 
+    open_button = tk.Button(root, text="Открыть CSV", command=lambda: open_csv(data_storage))
+    open_button.pack()
     root.mainloop()
-
-    print(data_storage.name_value.get(), data_storage.email_value.get(), data_storage.count_fine_value.get(), data_storage.deadline_fine_value.get())
-
